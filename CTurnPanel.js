d289f37959e91b847f27a8864de442484a1d4d2c
zackiscool000 <90630104+zackiscool000@users.noreply.github.com>
2021-10-28 12:16:38 -0400

    Update CTurnPanel.js

--- CTurnPanel.js
+++ CTurnPanel.js
@@ -1,61 +1,41 @@
-function CDisc(pStartPos,iType,oParentContainer){
-var _oWinEffect;
-var _oSprite;
+function CTurnPanel(iX,iY,oParentContainer){
+var _oDisc;
+var _oText;
 var _oContainer;
 var _oParentContainer;
 
-this._init = function(pStartPos,iType){
+this._init = function(iX,iY){
 _oContainer = new createjs.Container();
-_oContainer.x = pStartPos.x;
-_oContainer.y = pStartPos.y;
+_oContainer.x = iX;
+_oContainer.y = iY;
 _oParentContainer.addChild(_oContainer);
 
-var oSprite = s_oSpriteLibrary.getSprite("disc");
-var oData = {
-images: [oSprite],
-// width, height & registration point of each sprite
-frames: {width: oSprite.width/2, height: oSprite.height, regX: (oSprite.width/2)/2, regY: oSprite.height/2},
-animations: {state_0:[0],state_1:[1]}
+var oSpriteBg = s_oSpriteLibrary.getSprite("bg_turn");
+var oBg = createBitmap(oSpriteBg);
+_oContainer.addChild(oBg);
+
+var szText;
+if(s_iCurMode === MODE_CPU){
+szText = TEXT_PLAYER_TURN;
+}else{
+szText = TEXT_PLAYER1_TURN;
+}
+
+_oText = new createjs.Text(szText,"20px "+PRIMARY_FONT, "#fff");
+_oText.x = oSpriteBg.width/2;
+_oText.y = 36;
+_oText.textAlign = "center";
+_oText.textBaseline = "alphabetic";
+_oContainer.addChild(_oText);
+
+_oDisc = new CDisc({x:oSpriteBg.width/2,y:oSpriteBg.height/2 + 7},s_oGame.getCurTurn(),_oContainer);
 };
 
-var oSpriteSheet = new createjs.SpriteSheet(oData);
-
-_oSprite = createSprite(oSpriteSheet,"state_"+iType,(oSprite.width/4)/2,oSprite.height/2,oSprite.width/4,oSprite.height);
-_oContainer.addChild(_oSprite);
-
-var oSprite = s_oSpriteLibrary.getSprite("win_effect");
-var oData = {
-images: [oSprite],
-// width, height & registration point of each sprite
-frames: {width: 78, height: 78, regX: 39, regY: 39},
-animations: {stop:[0,0],anim:[0,27]}
-};
-
-var oSpriteSheetWin = new createjs.SpriteSheet(oData);
-_oWinEffect = createSprite(oSpriteSheetWin,"stop",39,39,78,78);
-_oWinEffect.x = 0;
-_oWinEffect.y = 0;
-_oContainer.addChild(_oWinEffect);
-};
-
-this.unload = function(){
-_oParentContainer.removeChild(_oContainer);
-};
-
-this.drop = function(iTime,pEndPos){
-//MOVE DISC TO THE END POSITION
-playSound("drop",1,0);
-createjs.Tween.get(_oContainer).to({y: pEndPos.y}, iTime,createjs.Ease.bounceOut).call(function(){s_oGame.checkCombos()});
-};
-
-this.playWinAnim = function(){
-_oWinEffect.gotoAndPlay("anim");
-};
-
-this.setFrame = function(iFrame){
-_oSprite.gotoAndStop("state_"+iFrame);
+this.setTurn = function(iTurn,szTurnText){
+_oText.text = szTurnText;
+_oDisc.setFrame(iTurn);
 };
 
 _oParentContainer = oParentContainer;
-this._init(pStartPos,iType);
+this._init(iX,iY);
 }
